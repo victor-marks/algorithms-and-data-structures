ce7c8750d0599d0c55b6e8f4b72be76e0177a126
Victor Marks <vjmarks@gmail.com>
2019-02-14 16:51:42 -0800

    optimized to exit early if already sorted after last pass through

--- sorting/bubbleSort.js
+++ sorting/bubbleSort.js
@@ -1,16 +1,20 @@
 function bubbleSort(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[i] > arr[j]) {
-        // let temp = arr[i];
-        // arr[i] = arr[j];
-        // arr[j] = temp;
-        [arr[i], arr[j]] = [arr[j], arr[i]];
+  let noSwaps;
+  for (let i = arr.length; i > 0; i--) {
+    noSwaps = true;
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        noSwaps = false;
       }
     }
+    if (noSwaps) break;
   }
   return arr;
 }
 
 bubbleSort([1, 3, 2, 4, 9, 5]);
 bubbleSort([3, -3, 0, 2, 1, -12, 9, 5, 7]);
+bubbleSort([8, 1, 2, 3, 4, 5, 6, 7]);
