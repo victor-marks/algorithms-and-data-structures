0c29ba96567a4f2521a78828948668004da22867
Victor Marks <vjmarks@gmail.com>
2019-03-07 13:02:39 -0800

    add to stack

---
+++ stacks/addToStack.js
@@ -0,0 +1,27 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  add(val) {
+    let newNode = new Node(val);
+    if (this.size === 0) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      let temp = this.first;
+      this.first = newNode;
+      this.first.next = temp;
+    }
+    return ++this.size;
+  }
+}
