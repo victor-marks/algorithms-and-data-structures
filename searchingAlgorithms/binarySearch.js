e1cc08b8807b8fa295a88067b3dce0e7f2ea9dd6
Victor Marks <vjmarks@gmail.com>
2019-03-08 16:00:09 -0800

    added Big O notation

---
+++ searchingAlgorithms/binarySearch.js
@@ -0,0 +1,101 @@
+/*
+Write a function called binarySearch which accepts a sorted array and a value and returns the index at which the value exists. Otherwise, return -1.
+Searching - O(log N)
+*/
+
+function binarySearch(arr, num) {
+  let left = 0;
+  let right = arr.length - 1;
+  let mid = Math.floor((left + right) / 2);
+
+  while (left <= right && arr[mid] !== num) {
+    if (num < arr[mid]) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+    mid = Math.floor((left + right) / 2);
+  }
+
+  return arr[mid] === num ? mid : -1;
+}
+
+binarySearch([1, 2, 3, 4, 5], 2); // 1
+binarySearch([1, 2, 3, 4, 5], 3); // 2
+binarySearch([1, 2, 3, 4, 5], 5); // 4
+binarySearch([1, 2, 3, 4, 5], 6); // -1
+binarySearch(
+  [
+    5,
+    6,
+    10,
+    13,
+    14,
+    18,
+    30,
+    34,
+    35,
+    37,
+    40,
+    44,
+    64,
+    79,
+    84,
+    86,
+    95,
+    96,
+    98,
+    99
+  ],
+  10
+); // 2
+binarySearch(
+  [
+    5,
+    6,
+    10,
+    13,
+    14,
+    18,
+    30,
+    34,
+    35,
+    37,
+    40,
+    44,
+    64,
+    79,
+    84,
+    86,
+    95,
+    96,
+    98,
+    99
+  ],
+  95
+); // 16
+binarySearch(
+  [
+    5,
+    6,
+    10,
+    13,
+    14,
+    18,
+    30,
+    34,
+    35,
+    37,
+    40,
+    44,
+    64,
+    79,
+    84,
+    86,
+    95,
+    96,
+    98,
+    99
+  ],
+  100
+); // -1
---
+++ searchingAlgorithms/naiveStringSearch.js
@@ -0,0 +1,18 @@
+/*
+Write a function called naiveSearch which accepts a string and a substring and returns how many times the substring occurs in the first string.
+Searching - O(n^2)
+*/
+
+function naiveSearch(long, short) {
+  let count = 0;
+
+  for (let i = 0; i < long.length; i++) {
+    for (let j = 0; j < short.length; j++) {
+      if (short[j] !== long[i + j]) break;
+      if (j === short.length - 1) count++;
+    }
+  }
+  return count;
+}
+
+naiveSearch('lorie loled', 'lol');
