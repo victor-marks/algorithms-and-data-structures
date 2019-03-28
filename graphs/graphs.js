836b1ba8c115a93011497f7de01c51a3dee6c8df
Victor Marks <vjmarks@gmail.com>
2019-03-28 13:07:16 -0700

    remove edge from graph

--- graphs/graphs.js
+++ graphs/graphs.js
@@ -11,9 +11,20 @@ class Graph {
     this.adjacencyList[v1].push(v2);
     this.adjacencyList[v2].push(v1);
   }
+
+  removeEdge(v1, v2) {
+    this.adjacencyList[v1] = this.adjacencyList[v1].filter(
+      vertex => vertex !== v2
+    );
+    this.adjacencyList[v2] = this.adjacencyList[v2].filter(
+      vertex => vertex !== v1
+    );
+  }
 }
 
 let g = new Graph();
 g.addVertex('Dallas');
 g.addVertex('Tokyo');
 g.addVertex('Aspen');
+g.addEdge('Dallas', 'Tokyo');
+g.addEdge('Dallas', 'Aspen');
