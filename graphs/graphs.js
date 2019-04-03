20a0d90c78f4dfb18ae6c55532d06899b92c8e75
Victor Marks <vjmarks@gmail.com>
2019-04-02 23:58:24 -0700

    DFS recursive

--- graphs/graphs.js
+++ graphs/graphs.js
@@ -28,6 +28,25 @@ class Graph {
     }
     delete this.adjacencyList[vertex];
   }
+
+  depthFirstRecursive(start) {
+    const result = [];
+    const visited = {};
+    const adjacencyList = this.adjacencyList;
+
+    (function dfs(vertex) {
+      if (!vertex) return null;
+      visited[vertex] = true;
+      result.push(vertex);
+      adjacencyList[vertex].forEach(neighbor => {
+        if (!visited[neighbor]) {
+          return dfs(neighbor);
+        }
+      });
+    })(start);
+
+    return result;
+  }
 }
 
 let g = new Graph();
