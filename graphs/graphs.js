db66058e25afa9a87142a70417de975ae4c2e15d
Victor Marks <vjmarks@gmail.com>
2019-04-03 11:09:27 -0700

    dfs iterative

--- graphs/graphs.js
+++ graphs/graphs.js
@@ -47,6 +47,27 @@ class Graph {
 
     return result;
   }
+
+  depthFirstIterative(start) {
+    const stack = [start];
+    const result = [];
+    const visited = {};
+    let currentVertex;
+
+    visited[start] = true;
+    while (stack.length) {
+      currentVertex = stack.pop();
+      result.push(currentVertex);
+
+      this.adjacencyList[currentVertex].forEach(neighbor => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          stack.push(neighbor);
+        }
+      });
+    }
+    return result;
+  }
 }
 
 let g = new Graph();
