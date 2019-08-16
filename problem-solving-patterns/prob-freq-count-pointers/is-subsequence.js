11b28895f810a67e69895d4422b1a7a28e451b23
Victor Marks <vjmarks@gmail.com>
2019-08-15 23:13:35 -0700

    isSubsequence with multiple pointers

--- problem-solving-patterns/prob-freq-count-pointers/is-subsequence.js
+++ problem-solving-patterns/prob-freq-count-pointers/is-subsequence.js
@@ -1,3 +1,26 @@
-// add whatever parameters you deem necessary
-function isSubsequence() {
+/*
+Write a function called isSubsequence which takes in two strings and checks whether 
+the characters in the first string form a subsequence of the characters in the second 
+string. In other words, the function should check whether the characters in the first 
+string appear somewhere in the second string, without their order changing. 
+
+isSubsequence('hello', 'hello world'); // true
+isSubsequence('sing', 'sting'); // true
+isSubsequence('abc', 'abracadabra'); // true
+isSubsequence('abc', 'acb'); // false (order matters)
+*/
+
+function isSubsequence(str1, str2) {
+  let i = 0;
+  let j = 0;
+
+  if (!str1) return true;
+  while (j < str2.length) {
+    if (str2[j] === str1[i]) i++;
+    if (i === str1.length) return true;
+    j++;
+  }
+  return false;
 }
+
+module.exports = { isSubsequence };
--- problem-solving-patterns/prob-freq-count-pointers/is-subsequence.test.js
+++ problem-solving-patterns/prob-freq-count-pointers/is-subsequence.test.js
@@ -1,13 +1,15 @@
-describe("isSubsequence", function() {
-  it("should return true if the first string is a substring of the second", function() {
-    expect(isSubsequence("hello", "hello world")).toBe(true);
-    expect(isSubsequence("sing", "sting")).toBe(true);
-    expect(isSubsequence("abc", "abracadabra")).toBe(true);
+const { isSubsequence } = require('./is-subsequence');
+
+describe('isSubsequence', function() {
+  it('should return true if the first string is a substring of the second', function() {
+    expect(isSubsequence('hello', 'hello world')).toBe(true);
+    expect(isSubsequence('sing', 'sting')).toBe(true);
+    expect(isSubsequence('abc', 'abracadabra')).toBe(true);
   });
 
-  it("should return true if the first string is not a substring of the second", function() {
-    expect(isSubsequence("abc", "acb")).toBe(false);
-    expect(isSubsequence("notasubstring", "banana")).toBe(false);
-    expect(isSubsequence("almost", "almossssss")).toBe(false);
+  it('should return true if the first string is not a substring of the second', function() {
+    expect(isSubsequence('abc', 'acb')).toBe(false);
+    expect(isSubsequence('notasubstring', 'banana')).toBe(false);
+    expect(isSubsequence('almost', 'almossssss')).toBe(false);
   });
 });
