301da6ff9df893928ef58a988515edcfee5903de
Victor Marks <vjmarks@gmail.com>
2019-08-18 11:39:17 -0700

    pivot index with sliding window

--- problem-solving-patterns/prob-freq-count-pointers/pivot-index.js
+++ problem-solving-patterns/prob-freq-count-pointers/pivot-index.js
@@ -1,3 +1,41 @@
-// add whatever parameters you deem necessary
-function pivotIndex() {
+/*
+Write a function called pivotIndex which accepts an array of integers, and returns the pivot index where 
+the sum of the items to the left equal to the sum of the items to the right. If there are more than one 
+valid pivot index, return the smallest value.
+
+Examples:
+
+pivotIndex([1,2,1,6,3,1]) // 3
+pivotIndex([5,2,7]) // -1  no valid pivot index
+pivotIndex([-1,3,-3,2]) // 1 valid pivot at 2: -1 + 3 = 2 however there is a smaller valid pivot at 1: -1 = -3 + 2
+Constraints
+
+Time Complexity: O(N)
+*/
+
+function pivotIndex(arr) {
+  let i = 0;
+  let j = 1;
+  let k = 2;
+  let leftSum = arr[i];
+  let targetSum = arr[j];
+  let rightSum = 0;
+  for (let l = 2; l < arr.length; l++) {
+    rightSum += arr[l];
+  }
+
+  while (k < arr.length) {
+    if (leftSum === rightSum) return j;
+    else {
+      i++;
+      j++;
+      k++;
+      leftSum += arr[i];
+      rightSum -= arr[j];
+    }
+  }
+
+  return -1;
 }
+
+module.exports = { pivotIndex };
